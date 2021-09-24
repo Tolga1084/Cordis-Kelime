bfddc9deadf66448d967f704288f15b73bc691ad
Tolga Gülsuna <39686884+Tolga1084@users.noreply.github.com>
2021-09-24 06:06:42 +0000

    lowercased dictionary added database

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:${
 */
 const { Client, Intents } = require('discord.js');
 const { PerformanceObserver, performance } = require('perf_hooks');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 const DISCORD_TOKEN = process.env['DISCORD_TOKEN'];
 const Database = require("@replit/database");
 
@@ -22,64 +22,61 @@ const Database = require("@replit/database");
 // Import dictionary.txt to array
 var fs = require("fs");
 var TR = fs.readFileSync("./TDK.txt");
-var dictionary = TR.toString().split("\n");
+var dictionary = TR.toString().toLowerCase().split("\n");
 
 
 // Sort Turkish Characters Correctly
-dictionary.sort(function (a, b) {
+dictionary.sort(function(a, b) {
   return a.localeCompare(b);
 });
 
 // database
-const db = new Database();
-
-
+const dictDB = new Database();
 
 // Initialise starting letter
 var startingLetter = dictionary[(Math.floor(Math.random() * dictionary.length))].toString().charAt(0);
-console.log('startingLetter is '+startingLetter);
+console.log('startingLetter is ' + startingLetter);
 
 
 // Javascript program to implement Binary Search for strings
 // Returns index of x if it is present in arr[],
 // else return -1
-function binarySearch(arr,x)
-{
-    let l = 0, r = arr.length - 1;
-        while (l <= r) {
-            let m = l + Math.floor((r - l) / 2);
-            let res = x.localeCompare(arr[m]);
-            // Check if x is present at mid
-            if (res == 0)
-                return m;
-   
-            // If x greater, ignore left half
-            if (res > 0)
-                l = m + 1;
-   
-            // If x is smaller, ignore right half
-            else
-                r = m - 1;
-        }
-   
-        return -1;
-}
- 
-function isLetter (str) {
-    let res = /^[a-zA-Z' 'wığüşöçĞÜŞÖÇİ]+$/.test(str);
-    console.log('isLetter '+res);
-    return res;
+function binarySearch(arr, x) {
+  let l = 0, r = arr.length - 1;
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+    let res = x.localeCompare(arr[m]);
+    // Check if x is present at mid
+    if (res == 0)
+      return m;
+
+    // If x greater, ignore left half
+    if (res > 0)
+      l = m + 1;
+
+    // If x is smaller, ignore right half
+    else
+      r = m - 1;
+  }
+
+  return -1;
 }
 
-function isOneWord (str) {
-    console.log('isOneWord input type is '+typeof str);
-    let res = (str.toString().trim().indexOf(' ') == -1)
-    console.log('isOneWord '+res);
-    return res;
+function isLetter(str) {
+  let res = /^[a-zA-Z' 'wığüşöçĞÜŞÖÇİ]+$/.test(str);
+  console.log('isLetter ' + res);
+  return res;
 }
 
-function checkStartingLetter (str,startingLetter) {
-    return str.startsWith(startingLetter.toLocaleLowerCase('tr'));
+function isOneWord(str) {
+  console.log('isOneWord input type is ' + typeof str);
+  let res = (str.toString().trim().indexOf(' ') == -1)
+  console.log('isOneWord ' + res);
+  return res;
+}
+
+function checkStartingLetter(str, startingLetter) {
+  return str.startsWith(startingLetter.toLocaleLowerCase('tr'));
 }
 
 
@@ -121,24 +118,24 @@ client.on('messageCreate', (message) => {
     return;
   }
 
-  if (!checkStartingLetter(word,startingLetter)) {
+  if (!checkStartingLetter(word, startingLetter)) {
     message.reply({
-      content: 'başlangıç harfi '+ `**${startingLetter.toLocaleUpperCase('tr')}**` +  ' olmalı! ' + `${altarSopali}`
+      content: 'başlangıç harfi ' + `**${startingLetter.toLocaleUpperCase('tr')}**` + ' olmalı! ' + `${altarSopali}`
     })
     let t1 = performance.now();
     console.log("replied in " + (t1 - t0) + " milliseconds.");
     return;
   }
 
-  
-  if (binarySearch(dictionary,word) == -1) {
-     message.reply(`${taam}
+
+  if (binarySearch(dictionary, word) == -1) {
+    message.reply(`${taam}
 son harf **${startingLetter.toLocaleUpperCase('tr')}**`);
   }
   else {
     message.react('✅');
-    startingLetter = word.slice(-1); 
-  } 
+    startingLetter = word.slice(-1);
+  }
   let t1 = performance.now();
   console.log("replied in " + (t1 - t0) + " milliseconds.");
 })
